4c37eea6a682795792e16d00cf99ed1b61ed64a7
Dmitry Dolgov <pahuukorppi@gmail.com>
2020-08-30 00:21:47 +0300

    Add bootstrap & redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import Card from 'react-bootstrap/Card';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const App = () => (
-  <div>Hello, Jeopardy!</div>
+  <div>
+    <Card>
+      <Card.Body>
+        Hello, Jeopardy!
+      </Card.Body>
+    </Card>
+  </div>
 );
 
 export default App;
